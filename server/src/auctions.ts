1ae5374354b8f993b47d4ed90aa6bb5c8cd08088
Jonatan Schneider <j.schneider1004@gmail.com>
2018-05-28 16:09:48 +0200

    Add database request

--- server/src/auctions.ts
+++ server/src/auctions.ts
@@ -1,5 +1,6 @@
 import { Express, Request, Response } from 'express';
 import { Collection } from 'mongodb';
+import { Auction } from './model/Auction';
 
 export class Auctions {
     static init(router: Express, auctionsCollection: Collection) {
@@ -9,8 +10,32 @@ export class Auctions {
         });
         router.get('/api/auctions/:id', function(req: Request, res: Response) {
             const query: Object = {_id: new ObjectID(id)};
-           // TODO: Implement database actions
-            res.status(200).send({data: []});
+            auctionsCollection.findOne(query)
+                .then((auction: Auction) => {
+                    if (auction !== null) {
+                        message = 'Successfully retrieved auction ' + id;
+                        data = new Auction( // TODO: maybe wrong Auction type?
+                            auction._id,
+                            auction.sellerId,
+                            auction.name,
+                            auction.description,
+                            auction.images,
+                            auction.manufacturerId,
+                            auction.color,
+                            auction.startingPrice,
+                            auction.endTime,
+                            auction.bids[auction.bids.length - 1]);
+                        status = 200;
+                    } else {
+                        message = 'Id ' + id + 'not found';
+                        status = 404;
+                    }
+                })
+                .catch((error: MongoError) => {
+                    message = 'Database error ' + error.code;
+                    status = 505;
+                });
+            res.status(200).send({data: [], message: message});
         });
     }
 }
