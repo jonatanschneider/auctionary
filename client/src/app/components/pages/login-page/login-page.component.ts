e0156f286a596693f85e83ba7fa0c3285a4e26d8
Lukas Kimpel <lukas.kimpel@mni.thm.de>
2018-05-28 22:25:39 +0200

    Add login handler Add click action

--- client/src/app/components/pages/login-page/login-page.component.ts
+++ client/src/app/components/pages/login-page/login-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginProvider } from '../../../models/LoginProvider';
+import { NotificationService } from '../../../services/util/notification.service';
 
 @Component({
   selector: 'app-login-page',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent implements OnInit {
+  LoginProvider = LoginProvider;
 
-  constructor() { }
+  constructor(private notificationService: NotificationService) { }
 
   ngOnInit() {
+  }
+
+  login(method: LoginProvider) {
+    switch (method) {
+      // ToDo: Add support for more login methods
+      case LoginProvider.GOOGLE:
+        document.location.href = '/auth/google';
+        break;
+      default:
+        this.notificationService.show('Login method not supported');
+    }
   }
 
 }
