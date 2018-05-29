89e4cd2e2f50f844449db01e31bca64d99ff0cd8
Lukas Kimpel <lukas.kimpel@mni.thm.de>
2018-05-29 08:59:58 +0200

    Add authentication service

---
+++ client/src/app/services/http/authentication.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { NotificationService } from '../util/notification.service';
+import { User } from '../../models/User';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
+import { of } from 'rxjs/internal/observable/of';
+import 'rxjs-compat/add/operator/map';
+import 'rxjs-compat/add/operator/catch';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+  user: User;
+
+  private apiUrl = {
+    base: 'https://localhost:8443/api',
+    user: '/user/'
+  };
+  private httpOptions = {
+    headers: new HttpHeaders({})
+  };
+
+  constructor(private notificationService: NotificationService,
+              private http: HttpClient) {
+  }
+
+  login(): Observable<boolean> {
+    const connectionUrl: string = this.apiUrl.base + this.apiUrl.user;
+
+    return this.http.get<User>(connectionUrl, this.httpOptions)
+      .map(user => {
+        this.user = user;
+        return true;
+      })
+      .catch(() => {
+        return of(false);
+      });
+  }
+}
