5c16e173e217f5545ee06beb1a67321932f032f4
Elias Kupferschmitt <elias.kupferschmitt@mni.thm.de>
2018-05-29 18:02:04 +0200

    Add google logout redirect

--- client/src/app/components/pages/logout-page/logout-page.component.ts
+++ client/src/app/components/pages/logout-page/logout-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {AuthenticationService} from "../../../services/http/authentication.service";
 import {NotificationService} from "../../../services/util/notification.service";
 import {Router} from "@angular/router";
+import {LoginProvider} from "../../../models/LoginProvider";
 
 @Component({
   selector: 'app-logout-page',
@@ -11,16 +12,25 @@ import {Router} from "@angular/router";
 export class LogoutPageComponent implements OnInit {
 
   constructor(private authenticationService: AuthenticationService,
-              private notificationService: NotificationService,
-              private router: Router) { }
+              private notificationService: NotificationService) { }
 
   ngOnInit() {
   }
 
   logout() {
-    this.authenticationService.logout().subscribe(() => {
-      this.notificationService.show("Successfully logged out");
-      this.router.navigate(['/']);
-    });
+    //let method: LoginProvider = this.authenticationService.user.login.type; //TODO: Fix and use this (user undefined)
+    let method: LoginProvider = LoginProvider.GOOGLE;
+
+    switch(method) {
+      case LoginProvider.GOOGLE:
+          this.authenticationService.logout().subscribe(() => {
+              this.notificationService.show("Successfully logged out");
+              document.location.href = "https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=https://localhost:8443/"
+          });
+          break;
+
+      default:
+        this.notificationService.show('Login method not supported');
+    }
   }
 }
