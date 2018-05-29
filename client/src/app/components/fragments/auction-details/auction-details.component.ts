cdec4224fb182ce859f39fd9d6db791a46299435
Jonatan Schneider <j.schneider1004@gmail.com>
2018-05-29 11:00:15 +0200

    Add interpolation directives for auction data

--- client/src/app/components/fragments/auction-details/auction-details.component.ts
+++ client/src/app/components/fragments/auction-details/auction-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Auction } from '../../../models/Auction';
 
 @Component({
   selector: 'app-auction-details',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auction-details.component.scss']
 })
 export class AuctionDetailsComponent implements OnInit {
+  public auction: Auction;
 
   constructor() {
   }
