e11f2d637eddae7fbf0ad7f26551baf7bad3e556
Jonatan Schneider <j.schneider1004@gmail.com>
2018-05-29 11:35:45 +0200

    Add getAuction

--- client/src/app/services/auction/auction.service.ts
+++ client/src/app/services/auction/auction.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
+import { Auction, Auction } from '../../models/Auction';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuctionService {
 
-  constructor() { }
+  private apiUrl = {
+    base: 'https://localhost:8443/api',
+    auction: '/auction'
+  };
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  getAuction(id: string): Observable<Auction> {
+    const connectionUrl: string = this.apiUrl.base + this.apiUrl.auction + id;
+    return this.http.get<Auction>(connectionUrl, {observe: response});
+  }
 }
