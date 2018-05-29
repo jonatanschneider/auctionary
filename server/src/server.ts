dad6d5b576fb5a8aff415e6570563096b4bef2ce
Lukas Kimpel <lukas.kimpel@mni.thm.de>
2018-05-29 09:01:05 +0200

    Fix mongo route initialization Fix wildcard middleware

--- server/src/server.ts
+++ server/src/server.ts
@@ -15,6 +15,21 @@ import { Profile } from 'passport';
 import { GoogleAuth } from './auth/GoogleAuth';
 import { Auctions } from './auctions';
 
+// Server constants
+const router = express();
+const privateKey = fs.readFileSync(__dirname + '/../sslcert/localhost.key', 'utf8');
+const certificate = fs.readFileSync(__dirname + '/../sslcert/localhost.crt', 'utf8');
+const credentials = { key: privateKey, cert: certificate };
+
+// Start https server
+https.createServer(credentials, router).listen(8443, function () {
+    console.log('HTTPS-server started on https://localhost:8443/');
+});
+
+// Configure router
+router.use(express.json());
+router.use(express.static(__dirname + './../dist'));
+
 // Database variables
 let appDb: Db;
 let auctionsCollection: Collection;
@@ -34,23 +49,6 @@ MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true })
         console.error('Connection to database failed:\n' + err);
     });
 
-// Server constants
-const router = express();
-const privateKey = fs.readFileSync(__dirname + '/../sslcert/localhost.key', 'utf8');
-const certificate = fs.readFileSync(__dirname + '/../sslcert/localhost.crt', 'utf8');
-const credentials = { key: privateKey, cert: certificate };
-
-// Start https server
-https.createServer(credentials, router).listen(8443, function () {
-    console.log('HTTPS-server started on https://localhost:8443/');
-});
-
-// Configure router
-router.use(express.json());
-
-// Publish dist folder
-router.use('/', express.static(__dirname + '/../dist'));
-
 // Authentication
 router.use(passport.initialize());
 router.use(passport.session());
@@ -67,4 +65,7 @@ const authConf = new AuthenticationConfig();
 function initRoutes(): void {
     GoogleAuth.init(passport, authConf, router);
     Auctions.init(router, auctionsCollection);
+
+    // Setup middleware redirection route
+    router.use('/*', express.static(__dirname + '/../dist'));
 }
