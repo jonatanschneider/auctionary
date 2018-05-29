b5281a07f7c95c2a2091fd4fd6b4d8b49a3204b1
Jonatan Schneider <j.schneider1004@gmail.com>
2018-05-29 12:16:01 +0200

    Add mapping for returning auction

--- client/src/app/services/auction/auction.service.ts
+++ client/src/app/services/auction/auction.service.ts
@@ -1,23 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/internal/Observable';
-import { Auction, Auction } from '../../models/Auction';
+import { Auction } from '../../models/Auction';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuctionService {
+  auction: Auction;
 
   private apiUrl = {
     base: 'https://localhost:8443/api',
     auction: '/auctions'
   };
 
-  constructor(private httpClient: HttpClient) {
+  constructor(private http: HttpClient) {
   }
 
   getAuction(id: string): Observable<Auction> {
     const connectionUrl: string = this.apiUrl.base + this.apiUrl.auction + '/' + id;
-    return this.http.get<Auction>(connectionUrl, {observe: response});
+    return this.http.get<Auction>(connectionUrl, {observe: 'response'})
+      .map(result => {
+        return result.body['auction'];
+      });
   }
 }
