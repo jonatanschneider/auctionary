d1586ab5b2c93be89ff9d48449257fd8f7163cda
Jonatan Schneider <j.schneider1004@gmail.com>
2018-05-31 11:56:55 +0200

    Revert everything

--- client/src/app/components/fragments/auction-details/auction-details.component.ts
+++ client/src/app/components/fragments/auction-details/auction-details.component.ts
@@ -11,19 +11,7 @@ import { AuctionService } from '../../../services/http/auction.service';
 export class AuctionDetailsComponent implements OnInit {
   auction: Auction;
 
-  constructor(private route: ActivatedRoute, public auctionService: AuctionService,
-              public bidDialog: MatDialog) {
-  }
-
-  openDialog(): void {
-    const dialogRef = this.bidDialog.open(BidDialogComponent, {
-      width: '250px',
-      data: {auction: this.auction}
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      // TODO bid actions
-    });
+  constructor(private route: ActivatedRoute, public auctionService: AuctionService) {
   }
 
   ngOnInit() {
