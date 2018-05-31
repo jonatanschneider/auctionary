96c25f79f007ad5601c3517f02697aa17eae6035
Jonatan Schneider <j.schneider1004@gmail.com>
2018-05-31 09:28:26 +0200

    Add openDialog and afterClosed to auction details

--- client/src/app/components/fragments/auction-details/auction-details.component.ts
+++ client/src/app/components/fragments/auction-details/auction-details.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Auction } from '../../../models/Auction';
 import { AuctionService } from '../../../services/auction/auction.service';
 import { ActivatedRoute } from '@angular/router';
+import { MatDialog } from '@angular/material';
+import { BidDialogComponent } from '../bid-dialog/bid-dialog.component';
 
 @Component({
   selector: 'app-auction-details',
@@ -12,7 +14,19 @@ export class AuctionDetailsComponent implements OnInit {
   public auction: Auction;
   private auctionID: string;
 
-  constructor(private route: ActivatedRoute, public auctionService: AuctionService) {
+  constructor(private route: ActivatedRoute, public auctionService: AuctionService,
+              public bidDialog: MatDialog) {
+  }
+
+  openDialog(): void {
+    const dialogRef = this.bidDialog.open(BidDialogComponent, {
+      width: '250px',
+      data: {auction: this.auction}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      // TODO bid actions
+    });
   }
 
   ngOnInit() {
