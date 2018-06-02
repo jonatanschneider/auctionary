7a60dee7ccef79bf296f74e305d854a127ab09e0
Unknown <jens.niederreiter@yahoo.de>
2018-06-02 16:15:00 +0200

    Add dependencies for Facebook

---
+++ server/src/auth/FacebookAuth.ts
@@ -0,0 +1,13 @@
+import * as pFacebook from 'passport-facebook';
+import * as request from 'request-promise';
+import { AuthenticationConfig } from './AuthenticationConfig';
+import { Express } from 'express';
+import { PassportStatic } from 'passport';
+import { Collection, MongoError } from 'mongodb';
+import { LoginProvider } from '../model/LoginProvider';
+import { User } from '../model/User';
+import { Login } from '../model/Login';
+
+export class FacebookAuth {
+
+}
