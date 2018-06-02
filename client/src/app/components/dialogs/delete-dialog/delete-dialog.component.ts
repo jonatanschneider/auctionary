2cc514b2e432ab22b09532a40bfc6b28ba7d4b66
Elias Kupferschmitt <elias.kupferschmitt@mni.thm.de>
2018-06-02 15:31:28 +0200

    Fix error (add data to delete-dialog component)

--- client/src/app/components/dialogs/delete-dialog/delete-dialog.component.ts
+++ client/src/app/components/dialogs/delete-dialog/delete-dialog.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { MatDialogRef } from '@angular/material';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 
 @Component({
   selector: 'app-delete-dialog',
@@ -8,7 +8,8 @@ import { MatDialogRef } from '@angular/material';
 })
 export class DeleteDialogComponent {
 
-  constructor(public dialogRef: MatDialogRef<DeleteDialogComponent>) {
+  constructor(public dialogRef: MatDialogRef<DeleteDialogComponent>,
+              @Inject(MAT_DIALOG_DATA) public data: any) {
   }
 
   onNoClick(): void {
